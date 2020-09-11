7e6fe8e5666925d07afd78c7dc94a9bde5df96d3
Alexander <xanderzinho26@gmail.com>
2020-09-11 15:35:38 -0300

    Adicionado componentes na tela principal

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,16 +1,90 @@
 import React from 'react';
+import { View, FlatList } from 'react-native';
 
 import Header from '../../components/Header';
+import Heading from '../../components/Heading';
+import Title from '../../components/Title';
+import CategoryList from '../../components/CategoryList';
+import StreamList from '../../components/StreamList';
+import ChannelList from '../../components/ChannelList';
 
 import { Wrapper, Container, Main } from './styles';
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
 
 const Following: React.FC = () => {
+  const { data, indices } = React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <Heading>Seguindo</Heading>,
+      },
+      {
+        key: 'FOLLOWED_CATEGORIES',
+        render: () => <Title>Categorias</Title>,
+        isTitle: true,
+      },
+      {
+        key: 'C1',
+        render: () => <CategoryList />,
+      },
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => <Title>Canais ao vivo</Title>,
+        isTitle: true,
+      },
+      {
+        key: 'C2',
+        render: () => <StreamList />,
+      },
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <Title>Continue assistindo</Title>,
+        isTitle: true,
+      },
+      {
+        key: 'C3',
+        render: () => <StreamList />,
+      },
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <Title>Canais offline</Title>,
+        isTitle: true,
+      },
+      {
+        key: 'C4',
+        render: () => <ChannelList />,
+      },
+    ];
+
+    const indices: number[] = [];
+
+    items.forEach((item, index) => item.isTitle && indices.push(index));
+
+    return {
+      data: items,
+      indices,
+    };
+  }, []);
+
   return (
     <Wrapper>
       <Container>
         <Header />
 
-        <Main />
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={({ item }) => item.render()}
+            keyExtractor={(item) => item.key}
+            stickyHeaderIndices={indices}
+            onRefresh={() => {}}
+            refreshing={false}
+          />
+        </Main>
       </Container>
     </Wrapper>
   );
