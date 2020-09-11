b51474edac8fd930f696e7772cb48ecdcc119f7a
Alexander <xanderzinho26@gmail.com>
2020-09-11 15:33:18 -0300

    Finalizado Lista de Categorias

---
+++ src/components/CategoryList/data.ts
@@ -0,0 +1,15 @@
+import lol from '../../images/category_lol.jpg';
+import valorant from '../../images/category_valorant.jpg';
+import csgo from '../../images/category_csgo.jpg';
+import fortnite from '../../images/category_fortnite.jpg';
+import fallguys from '../../images/category_fallguys.jpg';
+import apex from '../../images/category_apex.jpg';
+
+export default [
+  { name: 'League of Legends', source: lol, info: '117.1K' },
+  { name: 'VALORANT', source: valorant, info: '114.9K' },
+  { name: 'Counter-Strike: Global Offensive', source: csgo, info: '162.5K' },
+  { name: 'Fortnite', source: fortnite, info: '181.3K' },
+  { name: 'Fall Guys', source: fallguys, info: '50.9K' },
+  { name: 'Apex Legends', source: apex, info: '49.9K' },
+];
