80cd9c7afa4b1cf1566379b8685ee353f5ac761d
Alexander <xanderzinho26@gmail.com>
2020-09-11 15:35:11 -0300

    Finalizado Lista de Canais

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import {
+  List,
+  ChannelContainer,
+  LeftSide,
+  Avatar,
+  Column,
+  Username,
+  Info,
+  RightSide,
+  WhiteCircle,
+} from './styles';
+
+const ChannelList: React.FC = () => {
+  const ChannelItem = () => (
+    <ChannelContainer>
+      <LeftSide>
+        <Avatar />
+        <Column>
+          <Username>mixtuquenti</Username>
+          <Info>12 novos vídeos</Info>
+        </Column>
+      </LeftSide>
+
+      <RightSide>
+        <WhiteCircle />
+      </RightSide>
+    </ChannelContainer>
+  );
+  return (
+    <List>
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+    </List>
+  );
+};
+
+export default ChannelList;
