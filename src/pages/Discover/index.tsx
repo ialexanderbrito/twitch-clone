f2d31780f8b3bc5e8e032418f8b8e1c3cfce3261
Alexander <xanderzinho26@gmail.com>
2020-09-16 21:31:42 -0300

    Criado página de Descoberta

---
+++ src/pages/Discover/index.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Text, FlatList } from 'react-native';
+
+import Header from '../../components/Header';
+import Heading from '../../components/Heading';
+import Title from '../../components/Title';
+import CategoryList from '../../components/CategoryList';
+import StreamList from '../../components/StreamList';
+import ChannelList from '../../components/ChannelList';
+import VideoList from '../../components/VideoList';
+
+import { Wrapper, Container, Main } from './styles';
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
+
+const Discover: React.FC = () => {
+  const { data, indices } = React.useMemo(() => {
+    const items: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <Heading>Descubra</Heading>,
+      },
+      {
+        key: 'C1',
+        render: () => <VideoList />,
+      },
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => (
+          <Title>
+            <Text style={{ color: '#BF94FF' }}>Categorias</Text> que achamos que
+            vai gostar
+          </Title>
+        ),
+        isTitle: true,
+      },
+      {
+        key: 'C2',
+        render: () => <CategoryList />,
+      },
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <Title>Continue assistindo</Title>,
+        isTitle: true,
+      },
+      {
+        key: 'C3',
+        render: () => <StreamList />,
+      },
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <Title>Canais offline</Title>,
+        isTitle: true,
+      },
+      {
+        key: 'C4',
+        render: () => <ChannelList />,
+      },
+    ];
+
+    const indices: number[] = [];
+
+    items.forEach((item, index) => item.isTitle && indices.push(index));
+
+    return {
+      data: items,
+      indices,
+    };
+  }, []);
+
+  return (
+    <Wrapper>
+      <Container>
+        <Header />
+
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={({ item }) => item.render()}
+            keyExtractor={(item) => item.key}
+            stickyHeaderIndices={indices}
+            onRefresh={() => {}}
+            refreshing={false}
+          />
+        </Main>
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Discover;
