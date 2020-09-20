2431fab78f58245fc3ce7fc63ab510bbf98b690b
Alexander <xanderzinho26@gmail.com>
2020-09-19 21:05:26 -0300

    Adicionado cores Dark e Light

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+  primary: '#000000',
+  black: '#efeff1',
+  gray: '#7f7f8b',
+  tag: '#323235',
+  green: '#00E6CB',
+  red: '#E81916',
+  purple: '#BF94FF',
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,9 @@
+export default {
+  primary: '#f7f7f8',
+  black: '#000014',
+  gray: '#3a3a44',
+  tag: '#DEDEDF',
+  green: '#00E6CB',
+  red: '#E81916',
+  purple: '#BF94FF',
+};
