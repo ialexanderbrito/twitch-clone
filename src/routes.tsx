c46f994d8b178123a45904f9cfca3b47716bb4b4
Alexander <xanderzinho26@gmail.com>
2020-09-19 21:10:57 -0300

    Adicionado Dark mode e novos icones

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useSwitchTheme } from '../src/context/SwitchTheme';
 
 import Following from './pages/Following';
 import ComingSoon from './pages/ComingSoon';
@@ -11,110 +12,99 @@ import colors from './styles/colors';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
-const Routes: React.FC = () => (
-  <NavigationContainer>
-    <Navigator
-      tabBarOptions={{
-        style: {
-          height: 60,
-          backgroundColor: colors.primary,
-          borderTopWidth: 0,
-        },
-        tabStyle: {
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-        iconStyle: {
-          flex: 0,
-          width: 20,
-          height: 20,
-        },
-        labelStyle: {
-          fontFamily: 'roboto_400',
-          fontSize: 11,
-          marginTop: 5,
-        },
-        inactiveTintColor: colors.black,
-        activeTintColor: colors.purple,
-      }}
-    >
-      <Screen
-        name="Seguindo"
-        component={Following}
-        options={{
-          tabBarIcon: ({ size, focused }) => {
-            return (
-              <Ionicons
-                name="md-heart"
-                size={size}
-                color={focused ? colors.purple : colors.black}
-              />
-            );
+const Routes: React.FC = () => {
+  const { colors } = useSwitchTheme();
+
+  return (
+    <NavigationContainer>
+      <Navigator
+        tabBarOptions={{
+          style: {
+            height: 60,
+            backgroundColor: colors.primary,
+            borderTopWidth: 0,
           },
-        }}
-      />
-      <Screen
-        name="Descubra"
-        component={Discover}
-        options={{
-          tabBarIcon: ({ size, focused }) => {
-            return (
-              <MaterialCommunityIcons
-                name="compass-outline"
-                size={size}
-                color={focused ? colors.purple : colors.black}
-              />
-            );
+          tabStyle: {
+            alignItems: 'center',
+            justifyContent: 'center',
           },
-        }}
-      />
-      <Screen
-        name="Procurar"
-        component={ComingSoon}
-        options={{
-          tabBarIcon: ({ size, focused }) => {
-            return (
-              <Ionicons
-                name="ios-browsers"
-                size={size}
-                color={focused ? colors.purple : colors.black}
-              />
-            );
+          iconStyle: {
+            flex: 0,
+            width: 20,
+            height: 20,
           },
-        }}
-      />
-      <Screen
-        name="Esports"
-        component={ComingSoon}
-        options={{
-          tabBarIcon: ({ size, focused }) => {
-            return (
-              <MaterialCommunityIcons
-                name="trophy-outline"
-                size={size}
-                color={focused ? colors.purple : colors.black}
-              />
-            );
+          labelStyle: {
+            fontFamily: 'Roobert_400Regular',
+            fontSize: 11,
+            marginTop: 5,
           },
+          inactiveTintColor: colors.black,
+          activeTintColor: colors.purple,
         }}
-      />
-      <Screen
-        name="Transmitir"
-        component={ComingSoon}
-        options={{
-          tabBarIcon: ({ size, focused }) => {
-            return (
-              <Feather
-                name="radio"
-                size={size}
-                color={focused ? colors.purple : colors.black}
-              />
-            );
-          },
-        }}
-      />
-    </Navigator>
-  </NavigationContainer>
-);
+      >
+        <Screen
+          name="Seguindo"
+          component={Following}
+          options={{
+            tabBarIcon: ({ size, focused }) => {
+              return (
+                <MaterialCommunityIcons
+                  name="heart"
+                  size={size}
+                  color={focused ? colors.purple : colors.black}
+                />
+              );
+            },
+          }}
+        />
+        <Screen
+          name="Descubra"
+          component={Discover}
+          options={{
+            tabBarIcon: ({ size, focused }) => {
+              return (
+                <MaterialCommunityIcons
+                  name="compass-outline"
+                  size={size}
+                  color={focused ? colors.purple : colors.black}
+                />
+              );
+            },
+          }}
+        />
+        <Screen
+          name="Procurar"
+          component={ComingSoon}
+          options={{
+            tabBarIcon: ({ size, focused }) => {
+              return (
+                <MaterialCommunityIcons
+                  name="checkbox-multiple-blank-outline"
+                  size={size}
+                  color={focused ? colors.purple : colors.black}
+                />
+              );
+            },
+          }}
+        />
+        <Screen
+          name="Esports"
+          component={ComingSoon}
+          options={{
+            tabBarIcon: ({ size, focused }) => {
+              return (
+                <MaterialCommunityIcons
+                  name="trophy-outline"
+                  size={size}
+                  color={focused ? colors.purple : colors.black}
+                />
+              );
+            },
+          }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Routes;
